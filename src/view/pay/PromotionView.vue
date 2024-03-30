5f5feaef285307d75e59d287f3a548675eb75577
Claudio Espinoza <c.espinoza28@ufromail.cl>
2024-03-29 21:20:06 -0300

    Refactor: Update PromotionView

--- src/view/pay/PromotionView.vue
+++ src/view/pay/PromotionView.vue
@@ -1,9 +1,30 @@
 <script setup>
+import NavBar from '../../components/NavBar.vue';
 </script>
 
 <template>
-    <h1>Funciono</h1>
+  <article class="container"></article>
+
+  <NavBar />
+  <h1>Funciono</h1>
 </template>
 
 <style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+}
+
+.container {
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(
+    180deg,
+    rgba(65, 29, 86, 1) 0%,
+    rgba(35, 14, 49, 1) 25%,
+    rgba(15, 6, 21, 1) 50%,
+    rgba(6, 2, 9, 1) 75%,
+    rgba(0, 0, 0, 1) 100%
+  );
+}
 </style>
