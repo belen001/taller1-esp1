20c078344d0bdf7b9aa66ebe47dc0707445fae9f
Esteban Candia <E.candxa@gmail.com>
2024-03-30 21:54:32 -0300

    Avance con el diseño de la searchbar y searchresult

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,64 @@
 <template>
-  <input type="text" v-model="searchQuery" @input="onSearch" placeholder="Buscar canciones, artistas...">
+
+  <div class="general-container">
+
+    <div class="tittle-container"> {{titulo}} </div>
+
+    <div class="search-container">
+      <input
+          type="text"
+          v-model="searchQuery"
+          @input="onSearch"
+          class="search-input"
+          placeholder="Buscar canciones, artistas..."
+      >
+    </div>
+
+  </div>
+
 </template>
 
+<style scoped>
+.tittle-container{
+}
+
+.general-container{
+  display: flex;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  background: #5E4B8B;
+  border-radius: 20px;
+  padding: 5px 15px;
+  margin-left: 20%;
+}
+
+.search-input {
+  flex: 1;
+  background: transparent;
+  border: none;
+  color: white;
+  padding: 10px 0;
+  margin-left: 10px;
+}
+
+.search-input::placeholder {
+  color: rgba(255, 255, 255, 0.7);
+}
+
+.search-input:focus {
+  outline: none;
+}
+</style>
+
 <script>
-export default {
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SearchBar',
   data() {
     return {
       searchQuery: ''
@@ -13,6 +68,13 @@ export default {
     onSearch() {
       this.$emit('onSearch', this.searchQuery);
     }
+  },
+  props: {
+    titulo: {
+      type: String,
+      defaul: 'Inserte Titulo'
+    }
   }
-};
+});
 </script>
+
--- src/view/SearchResults.vue
+++ src/view/SearchResults.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import NavBar from '../components/NavBar.vue';
 import SearchBar from '../components/SearchBar.vue';
 import TopSongs from '../components/TopSongs.vue';
@@ -28,14 +28,52 @@ const filterResults = () => {
 </script>
 
 <template>
-  <NavBar />
-  <main class="container">
-    <SearchBar @onSearch="handleSearch" />
-    <TopSongs v-if="hasSearched" :songs="filteredSongs" />
-    <div class="h1">Pruebas</div>
-  </main>
+
+  <div class="general-container">
+
+    <NavBar />
+
+    <div class="search-header">
+      <SearchBar @onSearch="handleSearch" :titulo=" 'Busqueda' " />
+    </div>
+
+    <section v-if="hasSearched" class="search-results">
+
+      <div class="search-results__top-songs">
+        <h2>Top Songs</h2>
+        <TopSongs :songs="filteredSongs" />
+      </div>
+
+    </section>
+
+    <h1> Prueba</h1>
+
+  </div>
+
 </template>
 
 <style scoped>
 
+.general-container {
+  max-width: 1200px;
+  padding: 20px;
+  margin: 0 auto 0 28vw;
+}
+
+.search-header {
+}
+
+.search-results {
+}
+
+.search-results__top-songs
+{
+  margin-bottom: 20px;
+}
+
+.search-results h2 {
+  margin-bottom: 15px;
+}
+
 </style>
+
